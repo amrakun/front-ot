b61fbe5d620c6ae44ab94ef4aced66be69da2fa9
Hangarid <boljmor_garid@yahoo.com>
2018-01-10 14:42:53 +0800

    fix supplier base

--- src/modules/companies/components/list/Base.js
+++ src/modules/companies/components/list/Base.js
@@ -36,7 +36,7 @@ class Base extends Common {
       {
         title: 'Due dilligence',
         render: record =>
-          record.lastDueDiligence ? (
+          record.lastDueDiligence && record.lastDueDiligence.file ? (
             <a href={record.lastDueDiligence.file.url} target="_blank">
               Yes
             </a>
