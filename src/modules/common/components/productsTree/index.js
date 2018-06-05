55b01e2dce14f705d225405ef000004ae6cf84a7
batamar <batamar.b@nmtec.co>
2018-06-05 11:17:50 +0800

    Fixed productsInfo translation issue

--- src/modules/common/components/productsTree/index.js
+++ src/modules/common/components/productsTree/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { TreeSelect } from 'antd';
 import productsTree from './constants';
 
@@ -20,3 +21,7 @@ export default class ProductsTree extends React.Component {
     );
   }
 }
+
+ProductsTree.contextTypes = {
+  locale: PropTypes.string
+};
