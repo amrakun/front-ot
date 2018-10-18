937776b499af370eb3a29eaf7898d64123612514
batamar <batamar.b@nmtec.co>
2018-10-19 00:49:27 +0800

    Implemented logout functionality

--- src/modules/layout/utils.js
+++ src/modules/layout/utils.js
@@ -1,11 +1,24 @@
+import { gql } from 'react-apollo';
 import consts from 'consts';
+import client from 'apolloClient';
 
 export const logout = () => {
-  const { LOGIN_TOKEN_KEY, LOGIN_REFRESH_TOKEN_KEY } = consts;
+  client
+    .mutate({
+      mutation: gql`
+        mutation {
+          logout
+        }
+      `
+    })
 
-  localStorage.removeItem(LOGIN_TOKEN_KEY);
-  localStorage.removeItem(LOGIN_REFRESH_TOKEN_KEY);
-  localStorage.removeItem('locale');
+    .then(() => {
+      const { LOGIN_TOKEN_KEY, LOGIN_REFRESH_TOKEN_KEY } = consts;
 
-  window.location.href = '/';
+      localStorage.removeItem(LOGIN_TOKEN_KEY);
+      localStorage.removeItem(LOGIN_REFRESH_TOKEN_KEY);
+      localStorage.removeItem('locale');
+
+      window.location.href = '/';
+    });
 };
