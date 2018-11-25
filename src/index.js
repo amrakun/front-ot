3a0d1e8be5fb9915f0aa73f086b27b560c495006
batamar <batamar.b@nmtec.co>
2018-11-25 15:18:28 +0800

    Added babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import 'raf/polyfill';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/string';
-
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
