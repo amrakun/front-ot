f74da17208ab7976f32438afce075a39385deeaa
batamar <batamar.b@nmtec.co>
2018-11-25 15:10:04 +0800

    Changed import order

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'raf/polyfill';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/string';
-import 'raf/polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
