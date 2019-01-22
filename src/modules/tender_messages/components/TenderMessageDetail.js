3202ef1d1047a71eb7e13511ff77f9136f83341a
Dulguun Otgon <dulguun0225@gmail.com>
2019-01-22 13:55:25 +0800

    message attachment download hiih

--- src/modules/tender_messages/components/TenderMessageDetail.js
+++ src/modules/tender_messages/components/TenderMessageDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Tag, Divider, Input } from 'antd';
+import { Tag, Divider, Input, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+import { readFileUrl } from 'modules/common/utils';
 
 const { TextArea } = Input;
 
@@ -38,6 +40,20 @@ const Receivers = ({ recipientSuppliers, tender }) => {
   return <>To: {renderUser({ username: 'Tender', email: tender.number })}</>;
 };
 
+const FileLink = ({ attachment }) => {
+  if (!attachment || !attachment.url) return null;
+  else
+    return (
+      <>
+        <Divider />
+        <a href={readFileUrl(attachment.url)}>
+          <Icon type="paper-clip" />
+          Download attachment
+        </a>
+      </>
+    );
+};
+
 const TenderMessageDetail = ({ tenderMessageDetail }) => {
   if (!tenderMessageDetail) return null;
   return (
@@ -47,6 +63,7 @@ const TenderMessageDetail = ({ tenderMessageDetail }) => {
       <Receivers {...tenderMessageDetail} />
       <Divider />
       Subject: {tenderMessageDetail.subject}
+      <FileLink attachment={tenderMessageDetail.attachment} />
       <Divider />
       <TextArea readOnly autosize value={tenderMessageDetail.body} />
     </>
