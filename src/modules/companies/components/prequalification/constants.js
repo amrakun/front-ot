3771e146a2bcc9dbe651ca58d4d54c24bda6b6ed
BatAmar Battulga <batamar.b@nmtec.co>
2020-02-13 13:26:14 +0800

    changed fincncial year

--- src/modules/companies/components/prequalification/constants.js
+++ src/modules/companies/components/prequalification/constants.js
@@ -1,11 +1,7 @@
 export const yearData = [
+  { value: '2019', text: '2019' },
   { value: '2018', text: '2018' },
   { value: '2017', text: '2017' },
-  { value: '2016', text: '2016' },
-  { value: '2015', text: '2015' },
-  { value: '2014', text: '2014' },
-  { value: '2013', text: '2013' },
-  { value: '2012', text: '2012' },
 ];
 
 export const booleanData = [{ text: 'Yes', value: 'true' }, { text: 'No', value: 'false' }];
